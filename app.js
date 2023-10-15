677dd308fd242ac887bd70fc8f9e2caa701f9b8c
akhilreddy-24 <21eg107a24@anurag.edu.in>
2023-10-15 21:13:32 +0530

    gdgd

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('./app_server/models/db')
+require('./app_api/models/db')
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-
 var app = express();
+
+var routesApi = require('./app_api/routes/index');
+app.use('/api', routesApi);
 
 // view engine setup
 app.set('views', path.join(__dirname,'app_server', 'views'));
